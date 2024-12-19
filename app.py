dd5e651692404a639a3945f104dda9bff92579cf
AmirRaza16 <mdamir.raza016@gmail.com>
2024-12-19 17:19:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def download(client_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no PORT is specified
+    app.run(host="0.0.0.0", port=port)
